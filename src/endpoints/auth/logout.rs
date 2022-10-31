84af0b785482ca86f79181cada3dea7210d45e9e
Simon Johansson <simonjohansson312@gmail.com>
2022-10-31 10:46:13 +0100

    Removed unused code

--- src/endpoints/auth/logout.rs
+++ src/endpoints/auth/logout.rs
@@ -4,13 +4,10 @@
 
 use rocket::http::Status;
 use rocket::post;
-use rocket::serde::json::Json;
 use rocket_auth::*;
-use std::time;
 
 #[post("/logout")]
 pub async fn logout(auth: Auth<'_>) -> Status {
-    let one_hour = time::Duration::from_secs(60 * 60);
     let result = auth.logout();
     match result {
         Err(_) => Status::BadRequest,
