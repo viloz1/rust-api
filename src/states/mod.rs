7e498028c9621288b96af4febabe5461ad92af07
Simon Johansson <simonjohansson312@gmail.com>
2022-10-31 11:02:19 +0100

    Fixed import errors

--- src/states/mod.rs
+++ src/states/mod.rs
@@ -1,1 +1,16 @@
 pub mod processcomm;
+use crate::communication::protocols::Request;
+use crossbeam::channel::{Receiver, Sender};
+use rocket::fairing::AdHoc;
+
+/// Stage the states. Used in attach in the main Rocket launch. This is
+/// to make sure that Rocket manages the states.
+pub fn stage(tx: Sender<Request>, rx: Receiver<Request>) -> AdHoc {
+    AdHoc::on_ignite("States", |rocket| async {
+        let proc_com = processcomm::ProcessComm {
+            sender: tx,
+            receiver: rx,
+        };
+        rocket.manage(proc_com)
+    })
+}
